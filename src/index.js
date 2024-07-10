29c70811e0e12becd2e3b5557e1e346c302eb669
Sparsh <sparshgoelk@gmail.com>
2024-07-10 16:40:38 +0530

    setup basic env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
- 
+const express = require('express');
+const { PORT } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+  const app = express();
+
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+    // console.log(process.env.PORT);
+  });
+};
+
+setupAndStartServer();
