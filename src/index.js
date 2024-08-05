33b24bde7d4f8f3d3c4c91056c52392e5e763c21
Sparsh <sparshgoelk@gmail.com>
2024-08-06 00:09:24 +0530

    added files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
+  //create the express object
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
